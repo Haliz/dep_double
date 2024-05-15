e4458c4ad1047e1b22cbcabb4ff7e870fa44f6eb
Haliz <foril2n1@rambler.ru>
2024-05-15 21:21:04 +0300

    English comment

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once(__DIR__.'/step_request.php');
 
-error_reporting(0); // Отключение всех ошибок и предупреждений
+error_reporting(0); // Exception output ban
 
 require_once(__DIR__.'/search_logic.php'); 
 require_once(__DIR__.'/view_dublicates.php');
