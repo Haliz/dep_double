c3455ae4a5d99c63467677c666c5a726a49be0cd
Haliz <foril2n1@rambler.ru>
2024-05-15 21:12:37 +0300

    Запрет вывода ошибок

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?
 
 require_once(__DIR__.'/step_request.php');
+
+error_reporting(0); // Отключение всех ошибок и предупреждений
+
 require_once(__DIR__.'/search_logic.php'); 
 require_once(__DIR__.'/view_dublicates.php');
 
